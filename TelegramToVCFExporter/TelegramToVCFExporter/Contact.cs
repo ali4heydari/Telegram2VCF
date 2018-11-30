95b3d18f6c2096c3870b74f1baec0d41bf8ae3d0
Ali Heydari <ali4heydari@gmail.com>
2018-11-30 15:48:56 +0330

    Implement Browse and Export button click.

---
+++ TelegramToVCFExporter/TelegramToVCFExporter/Contact.cs
@@ -0,0 +1,27 @@
+﻿namespace TelegramToVCFExporter
+{
+    public class Contact
+    {
+        public string FirstAndLastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string VCF { get; set; }
+
+        public Contact(string firstAndLastName, string phoneNumber)
+        {
+            FirstAndLastName = firstAndLastName;
+            PhoneNumber = phoneNumber;
+            VCF =
+                $"BEGIN:VCARD" +
+                $"\nVERSION:2.1" +
+                $"\nN;CHARSET=UTF-8:{this.FirstAndLastName}" +
+                $"\nTEL;CELL:{this.PhoneNumber}" +
+                $"\nEND:VCARD";
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{nameof(FirstAndLastName)}: {FirstAndLastName}, {nameof(PhoneNumber)}: {PhoneNumber}, {nameof(VCF)}:\n{VCF}\n";
+        }
+    }
+}
